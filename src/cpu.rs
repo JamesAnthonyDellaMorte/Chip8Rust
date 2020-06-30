710bd1bf9b221cf21fcfe4c7a7a9988414ce0f91
JamesAnthonyDellaMorte <jamesanthony@delresearch.com>
2020-06-30 02:55:22 -0400

    Implemented some opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,17 +1,18 @@
+#![allow(dead_code)]
 pub struct cpu {
     // index register
     pub I: u16,
     // program counter
     pub pc: u16,
     // memory
-    pub memory: [u8; 4096],
+    pub memory: [u8; 0x1000],
     // registers
-    pub V: [u8; 16],
+    pub V: [u8; 0x10],
 
     // stack
-    pub stack: [u16; 16],
+    pub stack: [u16; 0x10],
     // stack pointer
-    pub sp: u8,
+    pub sp: usize,
     pub delay_timer: u8,
     pub sound_timer: u8,
 
@@ -24,9 +25,9 @@ impl cpu {
         pc: 0x200,
         I: 0,
         sp: 0,
-        memory: [0;4096],
-        V: [0; 16],
-        stack: [0; 16],
+        memory: [0;0x1000],
+        V: [0; 0x10],
+        stack: [0; 0x10],
         sound_timer: 0,
         delay_timer: 0,
         }
@@ -41,6 +42,39 @@ impl cpu {
        }
         println!("Game loaded");
 
+
+    }
+    pub fn CLS(&mut self)
+    {
+        println!("CLear Display");
+    }
+    pub fn RET(&mut self )
+    {
+        self.pc = self.stack[self.sp];
+        self.sp -= 1;
+
+    }
+    pub fn JP(&mut self, op: u16 )
+    {
+        self.pc = op & 0x0FFF;
+    }
+    pub fn CALL(&mut self, op: u16 )
+    {
+        self.stack[self.sp] = self.pc;
+        self.sp += 1;
+        self.pc = op & 0x0FFF;
+
+    }
+    pub fn SEVX(&mut self, op: u16)
+    {
+    if self.V[((op & 0x0F00) >> 8) as usize] == (op & 0x00FF) as u8
+    {
+        self.pc += 4;
+    }
+    else
+    {
+        self.pc += 2;
+    }
 
     }
 }
