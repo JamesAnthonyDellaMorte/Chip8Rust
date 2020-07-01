959d85a385b3041ef3f78aaea6436f4cecf1d20c
JamesAnthonyDellaMorte <jamesanthony@delresearch.com>
2020-06-30 21:49:33 -0400

    Added more OPCODES

--- src/cpu.rs
+++ src/cpu.rs
@@ -12,7 +12,7 @@ pub struct cpu {
     pub V: [u8; 0x10],
 
     // stack
-    pub stack: [u16; 0x10],
+    pub stack: [usize; 0x10],
     // stack pointer
     pub sp: usize,
     pub delay_timer: u8,
@@ -68,6 +68,15 @@ impl cpu {
             (0x05, _, _, _) => self.SE(opcode),
             (0x06, _, _, _) => self.LDVX(opcode),
             (0x07, _, _, _) => self.ADDVX(opcode),
+            (0x08, _, _, 0x0) => self.LD(opcode),
+            (0x08, _, _, 0x1) => self.OR(opcode),
+            (0x08, _, _, 0x2) => self.AND(opcode),
+            (0x08, _, _, 0x3) => self.XOR(opcode),
+            (0x08, _, _, 0x4) => self.ADD(opcode),
+            (0x08, _, _, 0x5) => self.SUB(opcode),
+            (0x08, _, _, 0x6) => self.SHR(opcode),
+            (0x08, _, _, 0x7) => self.SUBN(opcode),
+            (0x08, _, _, 0xE) => self.SHL(opcode),
             _ => self.NOP(),
         }
 
@@ -92,7 +101,7 @@ impl cpu {
     }
     pub fn CALL(&mut self, op: u16 )
     {
-        self.stack[self.sp] = self.pc as u16;
+        self.stack[self.sp] = self.pc as usize;
         self.sp += 1;
         self.pc = (op & 0x0FFF) as usize;
 
@@ -141,6 +150,88 @@ impl cpu {
     pub fn ADDVX(&mut self, op: u16)
     {
         self.V[((op & 0x0F00) >> 8) as usize] = self.V[((op & 0x0F00) >> 8) as usize] + (op & 0x00FF) as u8;
+        self.pc += 2;
+    }
+    pub fn LD(&mut self, op: u16)
+    {
+        self.V[((op & 0x0F00) >> 8) as usize] = self.V[((op & 0x00F0) >> 4) as usize];
+        self.pc += 2;
+    }
+    pub fn OR(&mut self, op: u16)
+    {
+        self.V[((op & 0x0F00) >> 8) as usize] = self.V[((op & 0x0F00) >> 8) as usize] | self.V[((op & 0x00F0) >> 4) as usize];
+        self.pc += 2;
+    }
+    pub fn AND(&mut self, op: u16)
+    {
+        self.V[((op & 0x0F00) >> 8) as usize] = self.V[((op & 0x0F00) >> 8) as usize] & self.V[((op & 0x00F0) >> 4) as usize];
+        self.pc += 2;
+    }
+    pub fn XOR(&mut self, op: u16)
+    {
+        self.V[((op & 0x0F00) >> 8) as usize] = self.V[((op & 0x0F00) >> 8) as usize] ^ self.V[((op & 0x00F0) >> 4) as usize];
+        self.pc += 2;
+    }
+
+    pub fn ADD(&mut self, op: u16)
+    {
+    let sum: u16= (self.V[((op & 0x0F00) >> 8) as usize] + self.V[((op & 0x00F0) >> 4) as usize]) as u16;
+    if sum > 0xFF
+    {
+        self.V[0xF] = 0x1;
+        self.V[((op & 0x0F00) >> 8) as usize]  = (sum & 0x00FF) as u8;
+    }
+    else
+    {
+        self.V[0xF] = 0;
+        self.V[((op & 0x0F00) >> 8) as usize] = sum as u8;
+    }
+        self.pc += 2;
+    }
+    pub fn SUB(&mut self, op: u16)
+    {
+        if self.V[((op & 0x0F00) >> 8) as usize] > self.V[((op & 0x00F0) >> 4) as usize]
+        {
+            self.V[0xF] = 0x1;
+        }
+        else {
+            self.V[0xF] = 0x0;
+        }
+        self.V[((op & 0x0F00) >> 8) as usize] -= self.V[((op & 0x00F0) >> 4) as usize];
+        self.pc += 2;
+
+    }
+    pub fn SHR(&mut self, op: u16)
+    {
+        if self.V[((op & 0x0F00) >> 8) as usize] & 1 == 1
+        {
+            self.V[0xF] = 0x1;
+        }
+        else {  self.V[0xF] = 0x0; }
+        self.V[((op & 0x0F00) >> 8) as usize] >>= 1;
+        self.pc += 2;
+    }
+    pub fn SUBN(&mut self, op: u16)
+    {
+        if self.V[((op & 0x0F00) >> 8) as usize] < self.V[((op & 0x00F0) >> 4) as usize]
+        {
+            self.V[0xF] = 0x1;
+        }
+        else {
+            self.V[0xF] = 0x0;
+        }
+        self.V[((op & 0x0F00) >> 8) as usize] = self.V[((op & 0x00F0) >> 4) as usize] - self.V[((op & 0x0F00) >> 8) as usize];
+        self.pc += 2;
+
+    }
+    pub fn SHL(&mut self, op: u16)
+    {
+        if self.V[((op & 0x0F00) >> 8) as usize] >> 7 == 1
+        {
+            self.V[0xF] = 0x1;
+        }
+        else {  self.V[0xF] = 0x0; }
+        self.V[((op & 0x0F00) >> 8) as usize] <<= 1;
         self.pc += 2;
     }
 
